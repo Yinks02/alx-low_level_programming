f1293589507ace20f7531a6e65c746404bd4f86d
Yinka <oluwayinkakolawole@gmail.com>
2023-06-22 05:12:49 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,16 +10,19 @@ void jack_bauer(void)
 	int i, j;
 
 	i = 0;
-
 	while (i < 24)
 	{
-		_putchar((i / 10) + '0');
-		_putchar((i % 10) + '0');
-		_putchar(':');
-		_putchar((j / 10) + '0');
-		_putchar((j % 10) + '0');
-		_putchar('\n');
-		j++;
+		 j = 0;
+		 while (j < 60)
+		 {
+			 _putchar((i / 10) + '0');
+			 _putchar((i % 10) + '0');
+			 _putchar(':');
+			 _putchar((j / 10) + '0');
+			 _putchar((j % 10) + '0');
+			 _putchar('\n');
+			 j++;
+		 }
+		 i++;
 	}
-	i++;
 }
